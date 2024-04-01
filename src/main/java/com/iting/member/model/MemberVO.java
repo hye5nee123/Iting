1fe07215e1b7ec9cd8deb976862d4777d8d02d81
tmdals <tmdals0145@naver.com>
2024-04-01 14:13:07 +0900

    2024_0401_도승민

---
+++ src/main/java/com/iting/member/model/MemberVO.java
@@ -0,0 +1,5 @@
+package com.iting.member.model;
+
+public class MemberVO {
+
+}
---
+++ src/main/java/com/iting/member/web/MemberController.java
@@ -0,0 +1,21 @@
+package com.iting.member.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.iting.member.model.MemberVO;
+
+@Controller
+public class MemberController {
+
+	@RequestMapping("member/main")
+	public ModelAndView getTestList(MemberVO vo) {
+		ModelAndView mv = new ModelAndView();
+		
+		
+		mv.setViewName("member/main");
+		return mv;
+	}
+	
+}
