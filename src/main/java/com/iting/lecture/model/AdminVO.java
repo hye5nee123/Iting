338747751d89729e5877c8ef42003b9a19371c40
sinsuji <sinsuji@naver.com>
2024-04-02 15:04:58 +0900

    240402_강의상세페이지_신수지

--- src/main/java/com/iting/lecture/model/AdminVO.java
+++
@@ -1,9 +0,0 @@
-package com.iting.lecture.model;
-
-import lombok.Data;
-
-@Data
-public class AdminVO {
-	private String title;
-	private String text;
-}
---
+++ src/main/java/com/iting/lecture/service/impl/LectureServiceImpl.java
@@ -0,0 +1,29 @@
+package com.iting.lecture.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iting.lecture.mapper.LectureMapper;
+import com.iting.lecture.model.LectureVO;
+import com.iting.lecture.service.LectureService;
+
+@Service
+public class LectureServiceImpl implements LectureService {
+
+	@Autowired
+	LectureMapper lectureMapper;
+
+	@Override
+	public List<LectureVO> getLectureList(LectureVO vo) {
+		return lectureMapper.getLectureList(vo);
+	}
+
+	@Override
+	public LectureVO getLectureInfo(String ltNum) {
+		return lectureMapper.getLectureInfo(ltNum);
+	}
+
+
+}
