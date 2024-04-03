a090eb87ebb45b784d9f3ef2f77f09c4f92ea451
zu9595 <qkrwldnddl02@naver.com>
2024-04-03 17:54:39 +0900

    240403_박지웅_세션값진행예정

---
+++ src/main/java/com/iting/common/security/CustomAuthFailureHandler.java
@@ -0,0 +1,24 @@
+package com.iting.common.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class CustomAuthFailureHandler implements AuthenticationFailureHandler {
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+		log.error("Login Fail Handler Alert...");
+		
+	}
+
+}
