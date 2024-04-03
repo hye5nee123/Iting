9cab510105d2cd16ce45d46c6a2bf5a02c1c60e7
sinsuji <sinsuji@naver.com>
2024-04-04 08:57:47 +0900

    240404_문제관리페이지작업_신수지

--- src/main/java/com/iting/test/mapper/TestMapper.java
+++ src/main/java/com/iting/test/mapper/TestMapper.java
@@ -9,5 +9,7 @@ import com.iting.test.model.TestVO;
 @Mapper
 public interface TestMapper {
 	public List<TestVO> getTestList();
+	public TestVO getTestInfo(String prblNum);
 	public int insertTest(TestVO vo);
+	public int deleteTest(String prblNum);
 }
--- src/main/java/com/iting/test/web/TestController.java
+++ src/main/java/com/iting/test/web/TestController.java
@@ -5,13 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.iting.cnq.model.CnqVO;
 import com.iting.test.model.TestVO;
 import com.iting.test.service.TestService;
 
@@ -50,4 +49,26 @@ public class TestController {
 		testService.insertTest(vo);
 		return vo;
 	}
+	
+	// 수정페이지 이동
+		@GetMapping("teacher/test/update/{prblNum}")
+		public String update(@PathVariable String prblNum, Model model) {
+			model.addAttribute("test", testService.getTestInfo(prblNum));
+			return "teacher/test/update";
+		}
+		
+		// 수정처리
+		@PostMapping("teacher/test/update/{prblNum}")
+		public String update(TestVO vo) {
+			testService.insertTest(vo);
+			return "redirect:teacher/test/list";
+		}
+			
+		// 삭제처리
+		@RequestMapping("teacher/test/delete/{prblNum}")
+		public String delete(@PathVariable String prblNum){ 
+			System.out.println("prblNum : " + prblNum);
+			testService.deleteTest(prblNum);
+			return "redirect:teacher/test/list";
+		}
 }
