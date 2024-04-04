d97bbf0bd39720e3c61b2764c18960437cf077b1
혜원 <152113818+hey5nee123@users.noreply.github.com>
2024-04-04 15:05:27 +0900

    20240404_수정중_조혜원

--- src/main/java/com/iting/common/web/CommonController.java
+++ src/main/java/com/iting/common/web/CommonController.java
@@ -1,8 +1,7 @@
 package com.iting.common.web;
 
+import java.security.Principal;
 
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
@@ -12,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.iting.common.service.UsersService;
@@ -22,48 +20,47 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 @Log4j2
 public class CommonController {
-	
+
 	@Autowired
 	UsersService userservice;
+
 	/* 메인이동 */
 	// 관리자
 	@RequestMapping("admin/main")
 	public ModelAndView goAdminMain() {
-		ModelAndView mv  = new ModelAndView("admin/main");
+		ModelAndView mv = new ModelAndView("admin/main");
 		return mv;
 	}
-	
+
 	// 회원
 	@RequestMapping("member/main")
 	public ModelAndView goMemberMain(Principal principal) {
 		// System.out.println(principal.getName()+"<<<<<<<<<<<<<<<<");
-		ModelAndView mv  = new ModelAndView("member/main");
+		ModelAndView mv = new ModelAndView("member/main");
 		return mv;
 	}
-	
+
 	// 강사
 	@RequestMapping("teacher/main")
 	public ModelAndView goTeacherMain() {
-		ModelAndView mv  = new ModelAndView("teacher/main");
+		ModelAndView mv = new ModelAndView("teacher/main");
 		return mv;
 	}
-	
-	
-	
+
 	/* 로그인 및 로그아웃 */
-	
-	//로그인 페이지 이동
+
+	// 로그인 페이지 이동
 	@GetMapping("/login")
 	public String loginForm() {
 		return "common/login";
 	}
-	
+
 //	@GetMapping("/loginfail")
 //	public void loginfail(Model model)	{
 //		model.addAttribute("failmessage", "아이디 혹은 비밀번호가 다릅니다");
 //	}
-	
-	//로그인 정보 체크
+
+	// 로그인 정보 체크
 //	@ResponseBody
 //	@GetMapping("/common/chkuser")
 //	public UsersVO dataChk(@RequestBody String id, Model model) {
@@ -71,8 +68,8 @@ public class CommonController {
 //		UsersVO vo = userservice.getUserInfo(id);
 //		return vo;
 //	}
-	
-	//로그인 정보 가져오기
+
+	// 로그인 정보 가져오기
 //	@RequestMapping("/userlogin")
 //	public void sessionLogin(String userid, HttpServletRequest request) {
 //		HttpSession session = request.getSession();
@@ -83,18 +80,19 @@ public class CommonController {
 //			session.setAttribute("userid", null);
 //		}
 //	}
-	
+
 	@GetMapping("/account")
 	public String accountForm() {
 		return "common/account";
 	}
-	
+
 	@GetMapping("/accessError")
 	public String accessDenied(Authentication auth, Model model) {
 		log.info("access denied :" + auth);
 		model.addAttribute("msg", "access denied");
 		return "common/accessError";
 	}
+
 	@ResponseBody
 	@GetMapping("/logout")
 	public String logout(HttpSession session) {
