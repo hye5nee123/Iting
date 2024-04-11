758eb4deed6aa2b29d94b7dc949011aa7f407b20
혜원 <152113818+hey5nee123@users.noreply.github.com>
2024-04-11 12:32:07 +0900

    20240411_save 구현중_조혜원

--- src/main/java/com/iting/cnq/service/ReplyService.java
+++ src/main/java/com/iting/cnq/service/ReplyService.java
@@ -6,7 +6,11 @@ import com.iting.cnq.model.CSearchVO;
 import com.iting.cnq.model.CnqVO;
 
 public interface ReplyService {
+	
 	// 목록 출력.
 	public Map<String, Object> getList(CnqVO vo, CSearchVO svo);
+
+	// 등록.
+	public int register(CnqVO vo);
 
 }
--- src/main/java/com/iting/cnq/web/ReplyController.java
+++ src/main/java/com/iting/cnq/web/ReplyController.java
@@ -2,6 +2,7 @@ package com.iting.cnq.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.iting.cnq.service.ReplyService;
 
@@ -10,6 +11,7 @@ public class ReplyController {
 
 	@Autowired
 	ReplyService replyService;
+	
 	
 
 	
